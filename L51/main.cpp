a8d5065b18037673e1817707b2e919eee3e24444
alvarohqr <alvarohqr@gmail.com>
2023-01-13 16:04:16 -0700

    Section 6 Challenge

---
+++ L51/main.cpp
@@ -0,0 +1,63 @@
+
+// Section 6
+// Challenge
+
+/*
+    Frank's Carpet Cleaning Service
+    Charges:
+        $25 per small room
+        $35 per large room
+    Sales tax rate is 6%
+    Estimates are valid for 30 days
+
+    Prompt the user for the number of small and large rooms they would like cleaned
+    and provide an estimate such as:
+
+Estimate for carpet cleaning service
+Number of small rooms: 3
+Number of large rooms: 1
+Price per small room: $25
+Price per large room: $35
+Cost : $110
+Tax: $6.6
+===============================
+Total estimate: $116.6
+This estimate is valid for 30 days
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout<< "Enter the number of small rooms: ";
+    int small_rooms{0};
+    cin >> small_rooms;
+
+    cout<< "Enter the number of large rooms: ";
+    int large_rooms{0};
+    cin >> large_rooms;
+
+    const int small_room_price{25};
+    const int large_room_price{35};
+
+    cout << "\n\nEstimate for carpet cleaning service" << endl;
+    cout << "Number of small rooms: " << small_rooms << endl;
+    cout << "Number of large rooms: " << large_rooms << endl;
+    cout << "Price per small room: " << small_room_price << endl;
+    cout << "Price per large room: " << large_room_price << endl;
+    int cost{0};
+    cost = (small_rooms * small_room_price) + (large_rooms * large_room_price);
+    cout << "Cost: $" << cost << endl;
+    const float tax{0.06};
+    cout << "Tax: $" << (float) cost*tax << endl;
+
+    cout << "===============================" << endl;
+    cout << "Total estimate: $" << cost + ((float) cost*tax) << endl;
+    const int days{30};
+    cout << "This estimate is valid for " << days <<" days" << endl;
+
+    return 0;
+}
+
